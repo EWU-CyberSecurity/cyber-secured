17677edcd2ddb96c07e3eec71dc9aca975d05010
Spencer Metz <spartanash1@gmail.com>
2021-10-22 12:49:21 -0700

    Updating latest googleSheet to unity code
    Working currently on the parsing to the screen
    Downloading is done
    Saving is going to be worked on last as i keep going back and forth were to save it.

---
+++ cyber-secured/Assets/Scripts/GoogleDataHandler/CSVManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CSVManager : MonoBehaviour
+{
+    public DownloadManager downMan;
+    public Parser parser;
+
+    private string topicsSheet;
+    private string questionsSheet;
+    void Start()
+    {
+        downMan.startDownloading();//Start the downloading of the sheets
+
+        //set the sheets here to send them to the parser and anywhere else we need.
+        topicsSheet = downMan.GetTopicTextFile(); 
+        questionsSheet = downMan.GetQuestionTextFile();
+        
+        parser.ReceivingSheets(topicsSheet, questionsSheet);
+        parser.Update(); // Call the update method in parser to print to the screen.
+    }
+}
--- cyber-secured/Assets/Scripts/GoogleDataHandler/DownloadManager.cs
+++ cyber-secured/Assets/Scripts/GoogleDataHandler/DownloadManager.cs
@@ -1,37 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class DownloadManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //change the "1isvQQd1lTvI0AewqS21pJmDxsEFiicxMb7-_zmlkhis" with what ever the new id is.
-    private string spreadsheetURL = "https://docs.google.com/spreadsheets/d/1isvQQd1lTvI0AewqS21pJmDxsEFiicxMb7-_zmlkhis/export?format=csv";
-    void Start()
-    {
-        StartCoroutine(GetText()); // at the  start of the game, start a coroutine to download the latest spreadsheet.
+    // https://www.youtube.com/watch?v=niOkw_cZEiM
+    private string textFileQuestionObj;
+    private string textFileTopicObj;
+    private int ctr = 1;
+    
 
-        //TODO in case the url breaks, go use a preexisting copy of the csv that is stored in the game already?
+    private static readonly string sheetID = "2PACX-1vTWkk_93-6edDxrGqz-gSsMgNmkVfIg_yQjIqypDpfpCw-rGIgYu5HqgK8ZHhA4SpzjGTrWh74DbLtD";
+    //readonly = final
+
+    // Start is called before the first frame update
+    private readonly string spreadsheet1URL = "https://docs.google.com/spreadsheets/d/e/" + sheetID + "/pub?output=csv&gid=932839450";   //QuestionObj gid - 932839450
+    private readonly string spreadsheet2URL = "https://docs.google.com/spreadsheets/d/e/" + sheetID + "/pub?output=csv&gid=2122133137";  //TopicObj gid - 2122133137
+
+    // https://stackoverflow.com/questions/24255472/download-export-public-google-spreadsheet-as-tsv-from-command-line/28494469#28494469
+    // URL format as of 10/21/21 - https://docs.google.com/spreadsheets/d/e/<sheetID>/pub?output=csv&gid=<GID>
+
+    public void startDownloading()
+    {
+        Debug.LogWarning("Start downloading");
+        StartCoroutine(DownloadText(spreadsheet1URL)); //QuestionObject
+        StartCoroutine(DownloadText(spreadsheet2URL)); //TopicObject
     }
 
-    IEnumerator GetText() //Grab the text verison of the csv. 
+    private IEnumerator DownloadText(string spreadsheetURL)
     {
-        UnityWebRequest www = UnityWebRequest.Get(spreadsheetURL); //make a UnityWebRequest with the spreadsheetURL
-        yield return www.SendWebRequest(); //send the request to download the file
+        UnityWebRequest www = UnityWebRequest.Get(spreadsheetURL);
+        yield return www.SendWebRequest();
 
-        if (www.error != null) // if error
+        if (www.error != null)
         {
-            Debug.Log(www.error); //log it to the console
+            Debug.Log(www.error);
+        } else {
+            Debug.LogWarning("Downloading sheet " + ctr);
+            //Debug.LogWarning(www.downloadHandler.text);
+            SetCSV(www.downloadHandler.text, ctr);
+            ctr++;
         }
-        else // else display the error to the console.
+    }
+    
+    //set the list depending on the ctr (1 is the question, 2 is the topics)
+    private void SetCSV(string newCSVText, int ctr)
+    {
+        if (ctr == 1)
         {
-            // Show results as text
-            Debug.LogWarning(www.downloadHandler.text);
-
-            //TODO save it or do something with the .text 
-
-            //I do not know what to do next...
+            textFileQuestionObj = newCSVText;
+            Debug.LogWarning(textFileQuestionObj);
+        } else {
+            textFileTopicObj = newCSVText;
+            Debug.LogWarning(textFileTopicObj);
         }
+    }
+    
+    public string GetQuestionTextFile()
+    {
+        return textFileQuestionObj;
+    }
+    public string GetTopicTextFile()
+    {
+        return textFileTopicObj;
     }
 }
