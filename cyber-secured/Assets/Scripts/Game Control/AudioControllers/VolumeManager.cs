fbf0ce00970c07e543814c37b8e8e7243fb9959d
MKieburtz <michaelkieburtz@gmail.com>
2021-10-24 13:47:51 -0700

    Fixed an issue where the volume slider didn't work

--- cyber-secured/Assets/Scripts/Game Control/AudioControllers/VolumeManager.cs
+++ cyber-secured/Assets/Scripts/Game Control/AudioControllers/VolumeManager.cs
@@ -10,9 +10,11 @@ public class VolumeManager : MonoBehaviour {
     private void Start()
     {
         musicSlider.value = 0.5f;
+        muteToggle = GameObject.Find("AudioToggle").GetComponent<MuteToggle>();
     }
 
     public void MusicSlider() {
+        muteToggle = GameObject.Find("AudioToggle").GetComponent<MuteToggle>();
         if (!muteToggle.getMuted())
         {
             AudioListener.volume = musicSlider.value;
