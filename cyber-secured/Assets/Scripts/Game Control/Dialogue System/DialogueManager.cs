3c3265340e71d00a08fe53ff8a96e6f096eab04d
ricky807 <80001920+ricky807@users.noreply.github.com>
2021-11-01 11:44:50 -0700

    Blue arrow bug (#21)
    * Bug with blue arrow
    
    Fixed Bug where you needed to double click the blue arrow and after that have to double click the continue button
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * Update cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>
    
    * cleanup
    
    Co-authored-by: Michael Kieburtz <michaelkieburtz@gmail.com>

--- cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
+++ cyber-secured/Assets/Scripts/Game Control/Dialogue System/DialogueManager.cs
@@ -25,58 +25,55 @@ using DG.Tweening;
 
 public class DialogueManager : MonoBehaviour
 {
-    //private MovingImagesAndText imagesAndText = new MovingImagesAndText(); // Object to call a function from that class when the user press continue button in the dialog
 
-    public GameObject dialogue_box;     //text box element to be dragged into inspector
-    public GameObject portrait;         //^
-    public Text text_name;              //^
-    public Text text_dialogue;          //^
+    public GameObject dialogue_box;     // textbox element to be dragged into inspector
+    public GameObject portrait;         // ^
+    public Text text_name;              // ^
+    public Text text_dialogue;          // ^
+
     public int currentSentenceDisplayed = -1; // to match the index in the queue, start in -1 and increase 
 
     public GameObject panel;
 
-    //private Queue<string> sentences;    //store lines of dialogue
     private ArrayList sentencesArrayList;
-    private Stack<string> sentencesStack; //store the lines to make sure I can read them again
+    private Stack<string> sentencesStack; // store the lines to make sure I can read them again
 
+    public float elapsed_time;          // keep track of time for lerping
+    public bool has_started;            // check dialogue's state
+    public bool has_ended;              // ^
 
-    public float elapsed_time;          //keep track of time for linear interpolation (lerp)
-    public bool has_started;            //check dialogue's state
-    public bool has_ended;              //^
+    public float x_pos_start_box        = 2.5f;     // the start position of each dialogue box component
+    public float x_pos_start_portrait   = -6.0f;    // ^
+    public float x_pos_end_box          = 16.0f;    // ^
+    public float x_pos_end_portrait     = -12.0f;   // ^
 
-    public float x_pos_start_box        = 2.5f;     //the start position of each dialogue box component
-    public float x_pos_start_portrait   = -6.0f;    //^
-    public float x_pos_end_box          = 16.0f;    //^
-    public float x_pos_end_portrait     = -12.0f;   //^
-
-    private bool proceed = true; //Create a locking mechanism to prevent the user to continue when demonstration in the one time pad is going 
+    private bool proceed = true; // create a locking mechanism to prevent the user to continue when demonstration in the one time pad is going 
     private bool finished_typing = false;
+    private bool pressedBackButton = false;
 
-    // Use this for initialization
+    // use this for initialization
     void Start()
     {
-        //sentences = new Queue<string>(); //FIFO system
-        sentencesStack = new Stack<string>(); //LIFO structure
+        sentencesStack = new Stack<string>(); // LIFO stracture
         sentencesArrayList = new ArrayList();
     }
 
-    // Allows for enter key to display next sentence
+    // allows for enter key to display next sentence
     void Update() 
     {
         if(!has_ended && has_started) 
         {
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) 
             {
-                DisplayNextSentenceArray();// it was DisplayNextSentence()
+                DisplayNextSentenceArray(); // it was DisplayNextSentence()
             }
         }
     }
-    //TODO: more comments
 
     public void StartDialogue(Dialogue dialogue)
     {
         Vector2 current_pos = dialogue_box.transform.position;
-        dialogue_box.transform.position = new Vector2(-16.0f, current_pos.y);    //reset position for lerp
+        dialogue_box.transform.position = new Vector2(-16.0f, current_pos.y); // reset position for lerp
         elapsed_time = 0;
         has_started = true;
         has_ended = false;
@@ -89,19 +86,19 @@ public class DialogueManager : MonoBehaviour
         foreach(string sentence in dialogue.sentences)
             sentencesArrayList.Add(sentence);
         
-        sentencesArrayList.Reverse(); // That will flip the list to start from the first sentence
+        sentencesArrayList.Reverse(); // that will flip the list to start from the first sentence
 
         DisplayNextSentenceArray(); // it was DisplayNextSentence()
     }
 
-    public void DisplayNextSentenceArray() //When the user clicks the button in the dialogue box
+    public void DisplayNextSentenceArray() // when the user clicks the button in the dialogue box
     {
-        if (proceed) // In some cases like in the demonstration of the one time pad - the user can't proceed until demonstration is over for each sentence
+        if (proceed) // in some cases like in the demonstration of the one time pad - the user can't proceed until demonstration is over for each sentence
         {
             // sentences haven't ended
             if (!has_ended)
             {
-                //are there more sentences in the array list?
+                // are there more sentences in the array list?
                 if (sentencesArrayList.Count == 0)
                 {
                     elapsed_time = 0;
@@ -117,53 +114,62 @@ public class DialogueManager : MonoBehaviour
 
             // play a beep sound
             GameObject.Find("SoundManager").GetComponent<AudioControllerV2>().PlaySound(3);
-
-            string sentence = (string)sentencesArrayList[sentencesArrayList.Count - 1]; // Give the last sentence
-            sentencesArrayList.RemoveAt(sentencesArrayList.Count - 1);// remove last element from the list
-            sentencesStack.Push(sentence); // To save it in the stack 
-
+            string sentence;
+            if(pressedBackButton) // this is for the case if the back button was pressed
+            {
+                sentence = (string)sentencesArrayList[sentencesArrayList.Count - 1]; // give the last sentence
+                sentencesArrayList.RemoveAt(sentencesArrayList.Count - 1); // remove last element from the list
+                sentencesStack.Push(sentence); // to save it in the stack 
+            }
+            sentence = (string)sentencesArrayList[sentencesArrayList.Count - 1]; // give the last sentence
+            sentencesArrayList.RemoveAt(sentencesArrayList.Count - 1); // remove last element from the list
+            sentencesStack.Push(sentence); // to save it in the stack 
+            pressedBackButton = false;
+            
             StopAllCoroutines();
-            //text_dialogue.text = sentence;
-            StartCoroutine(TypeSentence(sentence)); // You can disable that but you need to modify the demonstrations (10,11)
+            StartCoroutine(TypeSentence(sentence)); // you can disable that but you need to modify the demonstrations (10,11)
         }
     }
 
-    public void DisplayPrecedingSentence() //When the user clicks the button in the dialoue box
+    public void DisplayPrecedingSentence() // when the user clicks the button in the dialoue box
     {
-        if (proceed) // In some cases like in the demonstration of the one time pad - the user can't proceed until demonstration is over for each sentence
+        if (proceed) // in some cases like in the demonstration of the one time pad - the user can't proceed until demonstration is over for each sentence
         {
             // sentences haven't ended
             if (!has_ended)
             {
-                //are there more sentences in the stack?
+                // are there more sentences in the stack?
                 if (sentencesStack.Count == 0)
                 {
                     elapsed_time = 0;
                     has_ended = false; // was true
                     has_started = true; // was false
-
-                    // switch out of dialogue
-                    //GameControllerV2.Instance.DialogueSwitch(); 
-
                     return;
                 }
             }
 
             // play a beep sound
             GameObject.Find("SoundManager").GetComponent<AudioControllerV2>().PlaySound(3);
-
-            string sentence = sentencesStack.Pop();//giving the next sentence in the stack
+            string sentence;
+            if(!pressedBackButton) // first time pressing back
+            {
+                sentence = sentencesStack.Pop();  // gets rid of empty space
+                sentencesArrayList.Add(sentence); // to read it again when the user pressing next
+            }
+            
+            sentence = sentencesStack.Pop(); // giving the next sentence in the stack
             sentencesArrayList.Add(sentence); // to read it again when the user pressing next
-
+            
+            pressedBackButton = true;
             StopAllCoroutines();
             StartCoroutine(TypeSentence(sentence));
         }
     }
 
-    //looping through each character, creating a typewriter effect
-    IEnumerator TypeSentence(string sentence)//, byte fromArrayOrStack)//fromArrayOrStack - 0 from array 1 from stack
+    // looping through each character, creating a typewriter effect
+    IEnumerator TypeSentence(string sentence) 
     {
-        currentSentenceDisplayed++; // Updating the variable to show the index of the current sentence 
+        currentSentenceDisplayed++; // updating the variable to show the index of the current sentence 
 
         finished_typing = false;
 
@@ -174,7 +180,7 @@ public class DialogueManager : MonoBehaviour
             //yield return null; -----> If you want to type the sentence letter by letter you need to uncomment this
         }
 
-        //Check if both lengths of original sentence is the same as the sentence gets typed:
+        // check if both lengths of original sentence is the same as the sentence gets typed:
         if(text_dialogue.text.Length == sentence.Length)
         {
             finished_typing = true;
@@ -185,8 +191,7 @@ public class DialogueManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        // TODO: too messy, change to DOTween code (done partially)
-        // moving the text box smoothly
+        // moving the textbox smoothly
         if(has_started)
         {
             panel.GetComponent<Image>().raycastTarget = true;
@@ -200,19 +205,12 @@ public class DialogueManager : MonoBehaviour
                 current_pos,
                 new Vector2(x_pos_start_box, current_pos.y),
                0.5f * elapsed_time); 
-
-            //portrait start:
-            /*Vector2 current_pos_portait = portrait.transform.position;
-            portrait.transform.position = Vector2.Lerp(
-                current_pos_portait,
-                new Vector2(x_pos_start_portrait, current_pos_portait.y),
-                0.5f * elapsed_time);*/
             portrait.transform.DOMove(new Vector2(x_pos_start_portrait, current_pos.y), 0.8f);
         }
 
         if(has_ended)
         {
-            currentSentenceDisplayed = -1; // Updating the variable for the next iteration
+            currentSentenceDisplayed = -1; // updating the variable for the next iteration
 
             panel.GetComponent<Image>().raycastTarget = false;
             panel.GetComponent<Image>().DOColor(new Color32(50, 50, 50, 0), 0.5f);
@@ -225,13 +223,6 @@ public class DialogueManager : MonoBehaviour
                 current_pos,
                 new Vector2(x_pos_end_box, current_pos.y),
                 0.5f * elapsed_time);
-
-            //portrait end:
-            /*Vector2 current_pos_portait = portrait.transform.position;
-            portrait.transform.position = Vector2.Lerp(
-                current_pos_portait,
-                new Vector2(x_pos_end_portrait, current_pos_portait.y),
-                0.5f * elapsed_time);*/
             portrait.transform.DOMove(new Vector2(x_pos_end_portrait, current_pos.y), 0.5f);
         }
     }
