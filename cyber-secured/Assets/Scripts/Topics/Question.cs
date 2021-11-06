a0b31985dba11d90df7a980967a78b5597c6c200
MKieburtz <michaelkieburtz@gmail.com>
2021-11-05 20:58:59 -0700

    Added class for handling the button presses for custom topics

--- cyber-secured/Assets/Scripts/Topics/Question.cs
+++ cyber-secured/Assets/Scripts/Topics/Question.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Topics
 {
     /// <summary>
     /// This class represents a single question in a topic
     /// </summary>
-    class Question : TopicItem
+    class Question : TopicItem 
     {
         private string questionText;
         private Answer answer;
