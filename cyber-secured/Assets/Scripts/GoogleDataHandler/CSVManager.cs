caceb4d580ae6f8d9ab31809f87d77e99c8963bf
Spencer metz <10353164+spencerMetz@users.noreply.github.com>
2021-11-08 20:23:46 -0800

    Downloading data from a Google sheet to unity (#22)
    * updating the .gitignore
    
    added to ignore the .vsconfig
    
    * Adding latest downloadMaanger and updating the TESTSCENE.
    
    Current code that is working on itch.io
    Currently able to download the csv from google sheet into a text at runtime.
    Working on getting it to be saved and usable for the game.
    
    * Updating latest googleSheet to unity code
    
    Working currently on the parsing to the screen
    Downloading is done
    Saving is going to be worked on last as i keep going back and forth were to save it.
    
    * adding changes for parser
    
    * adding latest ideas to get data from google sheets
    
    Options are coroutine with Actions/Callbacks
    Or figure out how to use Async/wait to get the file
    
    * Updating branch with latest changes
    
    Still working on download not working
    
    * Adding fixed Downloader.
    
    Fixed issue with downloaded string not saving
    Cleaned up folders with unused scripts
    fixed main scene for CSVManager on GameControllerv2 Object.
    
    * Added review changes.
    
    Fixed formating in code.
    
    * Update CSVManager.cs
    
    * Clean up main scene based on what Spencer did
    
    Co-authored-by: MKieburtz <michaelkieburtz@gmail.com>

---
+++ cyber-secured/Assets/Scripts/GoogleDataHandler/CSVManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CSVManager : MonoBehaviour
+{
+    private string topicSheet;
+    private string questionSheet;
+
+    public CSVParser parser;
+
+    private static readonly string sheetID = "2PACX-1vTWkk_93-6edDxrGqz-gSsMgNmkVfIg_yQjIqypDpfpCw-rGIgYu5HqgK8ZHhA4SpzjGTrWh74DbLtD";  //Swap this out with the latest ID in the url if something breaks
+
+    private readonly string spreadsheet1URL = "https://docs.google.com/spreadsheets/d/e/" + sheetID + "/pub?output=csv&gid=932839450";   //QuestionObj gid - 932839450
+    private readonly string spreadsheet2URL = "https://docs.google.com/spreadsheets/d/e/" + sheetID + "/pub?output=csv&gid=2122133137";  //TopicObj gid - 2122133137
+
+    private void Start()
+    {
+        // Suppressing a C# warning, having it wont affect anything but figured it would be better to just not have it show up in console.
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        TaskHandlerAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+    }
+
+    async Task TaskHandlerAsync() 
+    {
+        // Download the CSVs from the urls above and then send the strings to the parser.
+        questionSheet = await GetText(spreadsheet1URL);
+        topicSheet = await GetText(spreadsheet2URL);
+
+        parser.RetrieveSheets(topicSheet, questionSheet);
+    }
+
+    // This is from https://github.com/mofrison/Unity3d-Network, modifying it may break stuff.
+    private static async Task<UnityWebRequest> SendWebRequest(UnityWebRequest request, 
+            CancellationTokenSource cancelationToken = null, System.Action<float> progress = null)
+    {
+        while (!Caching.ready)
+        {
+            if (cancelationToken != null && cancelationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+            await Task.Yield();
+        }
+        
+        request.SendWebRequest();
+
+        while (!request.isDone)
+        {
+            if (cancelationToken != null && cancelationToken.IsCancellationRequested)
+            {
+                request.Abort();
+                request.Dispose();
+
+                return null;
+            }
+            else
+            {
+                progress?.Invoke(request.downloadProgress);
+                await Task.Yield();
+            }
+        }
+        progress?.Invoke(1f);
+        return request;
+    }
+
+    // This is from https://github.com/mofrison/Unity3d-Network, modifying it may break stuff.
+    public static async Task<string> GetText(string url)
+    {
+        var uwr = await SendWebRequest(UnityWebRequest.Get(url));
+        if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
+        {
+            return uwr.downloadHandler.text;
+        }
+        else
+        {
+            throw new Exception("[Network] error: " + uwr.error + " " + uwr.url);
+        }
+    }
+}
