8b461fbfdeba9cdeab331c97398a6b602102c442
aydanmac <amackay1@ewu.edu>
2022-05-03 23:39:05 -0700

    Added Weapon and Enemy Framework
    Created scripts for an enemy base that can be expanded off of to dynamically create multiple enemy types, as well as the same for weapons to be applied to both enemies and the player. Actual functionality still ongoing.

---
+++ cyber-secured-testtime/Assets/Scripts/Cyber-Space/Enemies/EnemyBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase
+{
+    private int speed;
+    private int health;
+    private WeaponBase enemyWeapon;
+    private enum enemyTypes
+    {
+        ENEMY1,
+        ENEMY2,
+        ENEMY3
+    }
+
+    public int Speed
+    {
+        get{return speed;}
+        set{speed = value;}
+    }
+    public int Health
+    {
+        get{return health;}
+        set{health = value;}
+    }
+
+    public WeaponBase EnemyWeapon
+    {
+        get{return enemyWeapon;}
+        set{enemyWeapon = value;}
+    }
+
+    public void move()
+    {
+        //transform.Translate(Vector3(-1.0, 0.0, 0.0) * Time.deltaTime);
+    }
+
+    public void attack()
+    {
+
+    }
+}
---
+++ cyber-secured-testtime/Assets/Scripts/Cyber-Space/Weapons/WeaponBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase
+{
+    private int weaponID;
+    private string weaponName;
+    private int damage;
+    private float attackSpeed;
+    private int weaponEffectID;
+    
+    public int WeaponID
+    {
+        get{return weaponID;}
+        set{weaponID = value;}
+    }
+    public string WeaponName
+    {
+        get{return weaponName;}
+        set{weaponName = value;}
+    }
+
+    public int Damage
+    {
+        get{return damage;}
+        set{damage = value;}
+    }
+
+    public float AttackSpeed
+    {
+        get{return attackSpeed;}
+        set{attackSpeed = value;}
+    }
+
+    public int WeaponEffectID
+    {
+        get{return weaponEffectID;}
+        set{weaponEffectID = value;}
+    }
+}
